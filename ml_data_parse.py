c444ce5aac42f7cc6645377a11e4f9bd92aed3bd
nusbaoning <544124775@qq.com>
2017-11-03 20:22:31 +0800

    机器学习算法写好了

--- ml_data_parse.py
+++ ml_data_parse.py
@@ -8,12 +8,13 @@ import codecs
 import time
 import traceback
 
-datafile = 'fileserver_60s.log'
+
+datafile = 'fileserver_1h.log'
 HEADLINENUMBER = 11
 MINLENGH = 10
 READ_FUNCTION_NAME = "generic_file_read_iter"
-
-
+timeSep = 600
+timeCountMax = 4
 
 
 '''
@@ -26,9 +27,13 @@ def load_file(datafile):
     page = {}
     file = {}
     process = {}
-    reflection = {}
-    uniqueInode = 0
+    # reflection = {}
+    # uniqueInode = 0
     req = 1
+    currentTime = -1
+    
+    record = True    
+    timeCount = 0
     try:
         
         # with codecs.open(filename, 'r', encoding='utf-8', errors='ignore') as fin:
@@ -62,6 +67,22 @@ def load_file(datafile):
             task = line[0:task_tail_loc].strip()
             pid = int(line[task_tail_loc+1:cpu_loc])
             timestamp = float(line[timestamp_loc:function_loc].replace(':',''))
+            if currentTime<0:
+                currentTime = int(timestamp)
+                timeCount += 1
+            elif timestamp - currentTime >= timeSep:
+                record = not record
+                currentTime = int(timestamp)
+                # 完成一次记录+标注，需要输出+初始化
+                if record:
+                    output(page, process, file)
+                    page = {}
+                    file = {}
+                    process = {}
+                    req = 1                    
+                timeCount += 1
+                if timeCount > timeCountMax:
+                    break
             # print(task, pid, timestamp)
             line = line[function_loc:]
             colon_loc = line.find(':')
@@ -97,33 +118,41 @@ def load_file(datafile):
 #                 break
             
             #映射inode
-            if not inode in reflection:
-                reflection[inode] = {filename:uniqueInode}                
-                inode = uniqueInode
-                uniqueInode += 1
-            elif not filename in reflection[inode]:
-                reflection[inode][filename] = uniqueInode
-                inode = uniqueInode
-                uniqueInode += 1
+            # if not inode in reflection:
+            #     reflection[inode] = {filename:uniqueInode}                
+            #     inode = uniqueInode
+            #     uniqueInode += 1
+            # elif not filename in reflection[inode]:
+            #     reflection[inode][filename] = uniqueInode
+            #     inode = uniqueInode
+            #     uniqueInode += 1
+            # else:
+            #     inode = reflection[inode][filename]
+            if record:
+                if not inode in file.keys():
+                    file[inode] = filename
+                # fileserver.f中没有重命名指令，filename不同说明文件被删除，那么所有历史数据一并删除，开始记录新的访问请求
+                elif filename != file[inode]:
+                    file[inode] = filename
+                    del page[inode]
+                if not inode in page.keys():
+                    page[inode] = {}
+                index0 = pos >> 12
+                if r==0:
+                    count = min(count, isize-pos)
+                index1 = (pos + count - 1) >> 12
+                for index in range(index0, index1+1):
+                    access_page(page[inode], index, req, r, pid)
+                    access_process(inode, index, pid, r, process)
+                    req += 1
             else:
-                inode = reflection[inode][filename]
+#               原来的文件被删除或者出现了新page的访问，不用记录
+                if not inode in file.keys():
+                    continue
+                if not index in page[inode]:
+                    continue
+                tag_access_page(page[inode], index, r)
 
-            if not inode in file.keys():
-                file[inode] = filename
-            # fileserver.f中没有重命名指令，filename不同说明文件被删除，那么所有历史数据一并删除，开始记录新的访问请求
-            # elif filename != file[inode]:
-            #     file[inode] = filename
-            #     del page[inode]
-            if not inode in page.keys():
-                page[inode] = {}
-            index0 = pos >> 12
-            if r==0:
-                count = min(count, isize-pos)
-            index1 = (pos + count - 1) >> 12
-            for index in range(index0, index1+1):
-                access_page(page[inode], index, req, r, pid)
-                access_process(inode, index, pid, r, process)
-                req += 1
             if i%10000==0:
                 e = time.time()
                 print(i, int(100*i/lineNum), "%", "consumed", (e-s), "s")
@@ -131,49 +160,16 @@ def load_file(datafile):
     except Exception as e:
         print(i, line)
         print(len(line))
-#        print ('str(Exception):\t', str(Exception))
-#        print ('str(e):\t\t', str(e))
         print (repr(e))
-#        print ('e.message:\t', e.message)
         print (traceback.print_exc())
-        print (traceback.format_exc())
     finally:
-        fout = open(datafile+".parse", "w")
-        for inode in page.keys():
-            loc = file[inode].rfind('.')
-            if loc==-1:
-                fileType = None
-            else:
-                fileType = file[inode][loc+1:]
-            for index in page[inode].keys():                      
-                (req, reUseList, lR, lW, pidList) = page[inode][index]
-                if reUseList!=[]:
-                    minr = min(reUseList)
-                    maxr = max(reUseList)
-                    avg = sum(reUseList)/len(reUseList)
-                else:
-                    minr = maxr = avg = -1
-                l=[[],[],[],[]]
-                for pid in pidList:
-                    (keyList, r, w) = process[pid]
-                    avgr = r/len(keyList)
-                    avgw = w/len(keyList)
-                    l[0].append(r)
-                    l[1].append(w)
-                    l[2].append(avgr)
-                    l[3].append(avgw)
-                print(inode, index, fileType, file[inode], req, minr, 
-                      maxr, avg, lR, lW, lR+lW, len(pidList), min(l[0]), max(l[0]), sum(l[0])/len(l[0]),
-                      min(l[1]), max(l[1]), sum(l[1])/len(l[1]),
-                      min(l[2]), max(l[2]), sum(l[2])/len(l[2]),
-                      min(l[3]), max(l[3]), sum(l[3])/len(l[3]), sep=',',file=fout)
         
-        fin.close()
-        fout.close()
-        fout = open(datafile+".metadata", "w")
-        print(process, file=fout)
-        print(reflection, file=fout)
-        fout.close()
+        
+        fin.close()        
+#         fout = open(datafile+".metadata", "w")
+#         print(process, file=fout)
+# #        print(reflection, file=fout)
+#         fout.close()
         
 def access_page(inodeDict, index, req, rw, pid):
     if index not in inodeDict:
@@ -194,6 +190,94 @@ def access_process(inode, index, pid, rw, process):
         if key not in keyList:
             keyList.append(key)
         process[pid] = (keyList, r+1-rw, w+rw)
+
+def tag_access_page(inodeDict, index, r):
+    if len(inodeDict[index]) <= 5:
+        (lastreq, reUseList, lR, lW, pidList) = inodeDict[index]
+        inodeDict[index] = (lastreq, reUseList, lR, lW, pidList, 1-r, r)
+    else:
+        (lastreq, reUseList, lR, lW, pidList, ftR, ftW) = inodeDict[index]
+        inodeDict[index] = (lastreq, reUseList, lR, lW, pidList, ftR+1-r, ftW+r)
+
+def output(page, process, file):
+    fout = open(datafile[:-4]+"_" + str(timeSep) + "s_" + str(timeCountMax) + ".parse", "a")
+    i = 0
+    print("pid#=", len(process))
+
+# 获得good data的throttling
+    l = []
+    lrulist = []
+    for inode in page.keys():
+        for index in page[inode].keys():
+            tupleValue = page[inode][index]
+            lrulist.append(tupleValue[0])
+            if len(tupleValue) <= 5:
+                l.append(0)
+            else:
+                l.append(tupleValue[5]+tupleValue[6])
+    l.sort(reverse=True)
+    throttling = l[int(0.2*len(l))]
+    print("20%=", throttling, l[0], l[-1], l[int(0.1*len(l))])
+    throttling = max(throttling, 2)
+    print("final throt=", throttling)
+    lrulist.sort(reverse=True)
+    lruthrot = lrulist[int(0.02*len(l))]
+    print("lru throt=", lruthrot)
+    print("len=", len(l), len(lrulist), lrulist[0], lrulist[-1])
+    lruright = 0
+    for inode in page.keys():
+        loc = file[inode].rfind('.')
+        if loc==-1:
+            fileType = None
+        else:
+            fileType = file[inode][loc+1:]
+        
+        for index in page[inode].keys():
+            i += 1
+            tupleValue = page[inode][index]
+            if len(tupleValue) <= 5:
+                accessType=""
+                ftR=ftW=0
+                (req, reUseList, lR, lW, pidList) = page[inode][index]
+                dataType = False
+            else:
+                (req, reUseList, lR, lW, pidList, ftR, ftW) = page[inode][index]
+                readRatio = ftR/(ftR+ftW)
+                if readRatio >= 0.95:
+                    accessType = "R"
+                elif readRatio <= 0.05:
+                    accessType = "W"
+                else:
+                    accessType = "C"
+                dataType = (ftR+ftW>=throttling)
+            if reUseList!=[]:
+                minr = min(reUseList)
+                maxr = max(reUseList)
+                avg = sum(reUseList)/len(reUseList)
+            else:
+                minr = maxr = avg = 10**15
+            l=[[],[],[],[]]
+            for pid in pidList:
+                (keyList, r, w) = process[pid]
+                avgr = r/len(keyList)
+                avgw = w/len(keyList)
+                l[0].append(r)
+                l[1].append(w)
+                l[2].append(avgr)
+                l[3].append(avgw)
+            
+            if req >= lruthrot and dataType:
+                lruright += 1
+
+            print(inode, index, fileType, file[inode], req, minr, 
+                  maxr, avg, lR, lW, lR+lW, len(pidList), min(l[0]), max(l[0]), sum(l[0])/len(l[0]),
+                  min(l[1]), max(l[1]), sum(l[1])/len(l[1]),
+                  min(l[2]), max(l[2]), sum(l[2])/len(l[2]),
+                  min(l[3]), max(l[3]), sum(l[3])/len(l[3]), ftR, ftW, ftR+ftW, accessType, dataType, sep=',',file=fout)
+    print("total number of lines=", i)
+    print("lruright=", lruright, ",ratio=", round(100*lruright/i, 2), "%")
+    fout.close()
+
 
 start = time.time()
 load_file(datafile)
---
+++ ml_knn.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  3 19:27:56 2017
+
+@author: Bao Ning
+"""
+import pandas
+from sklearn import model_selection
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn import preprocessing
+
+# Load dataset
+#print(inode, index, fileType, file[inode], req, minr, 
+#                      maxr, avg, lR, lW, lR+lW, len(pidList), min(l[0]), max(l[0]), sum(l[0])/len(l[0]),
+#                      min(l[1]), max(l[1]), sum(l[1])/len(l[1]),
+#                      min(l[2]), max(l[2]), sum(l[2])/len(l[2]),
+##                      min(l[3]), max(l[3]), sum(l[3])/len(l[3]), sep=',',file=fout)
+names = ['inode', 'index', 'filetype', 'filename', 'latest access time', 
+         'min access distance','max access distance', 'avg access distance', 'read req', 'write req', 
+         'total req', 'process number', 'min process read', 'max process read','avg process read',
+          'min process write', 'max process write', 'avg process write', 'min process per page read', 'max process per page read',
+         'avg process per page read', 'min process per page write', 'max process per page write','avg process per page write', 'future read',
+          'future write', 'future total', 'access type', 'data type']
+dataset = pandas.read_csv("fileserver_10m_60s_4.parse", names=names, header=None)
+#print(dataset.columns)
+#print(dataset.head(20))
+deletedcolumns = ['index', 'future read', 'future write', 'future total', 'access type']
+for col in deletedcolumns:
+	dataset.drop(col,axis=1,inplace=True)
+columns = ['inode', 'filetype', 'filename']
+for col in columns:
+    le = preprocessing.LabelEncoder()
+    le.fit(dataset[col])
+    dataset[col] = le.transform(dataset[col])
+
+#url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+#names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+#dataset = pandas.read_csv(url, names=names)
+#print(dataset.shape)
+#print(dataset.head(20))
+#print(dataset.describe())
+#print(dataset.groupby('data type').size())
+#print(dataset.groupby('access type').size())
+#array = dataset.values
+#X = array[:,:-1]
+#Y = list(array[:,-1])
+##print(len(X[0]))
+##print(Y)
+#validation_size = 0.5
+array = dataset.values
+X = array[0:565033,0:-1]
+Y = list(array[0:565033,-1])
+validation_size = 0.50
+seed = 7
+X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
+seed = 7
+scoring = 'accuracy'
+#knn = KNeighborsClassifier()
+#knn.fit(X_train, Y_train)
+#predictions = knn.predict(X_validation)
+#print(accuracy_score(Y_validation, predictions))
+#print(confusion_matrix(Y_validation, predictions))
+#print(classification_report(Y_validation, predictions))
+knn = LogisticRegression()
+knn.fit(X_train, Y_train)
+predictions = knn.predict(X_validation)
+print(accuracy_score(Y_validation, predictions))
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
